104d78de22c6a57a630e418c508c62dfaf4444ed
mothibalaajiv <mothibalaaji@gmail.com>
2026-06-01 09:44:01 +0530

    Frontend ready for deployment

--- frontend/assets/config.js
+++ frontend/assets/config.js
@@ -3,4 +3,4 @@
 // After deploying the backend to Render,
 // replace it with your live Render URL, for example:
 // window.PULPBAE_API_URL = "https://pulpbae-api.onrender.com";
-window.PULPBAE_API_URL = "http://localhost:5000";
+window.PULPBAE_API_URL = "https://pulpbae.onrender.com/";
